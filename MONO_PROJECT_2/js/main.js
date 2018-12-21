de2f83e571d05832f2e337a242f62dcd42effc65
asmail11 <imdrmas@gmail.com>
2018-12-21 10:48:05 +0100

    My Project Fredend Simplon

--- MONO_PROJECT_2/js/main.js
+++ MONO_PROJECT_2/js/main.js
@@ -1,39 +1,69 @@
 $(document).ready(function() {
 
-
-  $('.pill-nav a').click(function () {
-  $(this).addClass('active').parent().siblings().find('a').removeClass('active');
-// $(this).addClass('active');
-//  $(this).removeClass('active');
-
-});
-
-var scrollToTop = $('.scroll-to-top');
- if ($(window).scrollTop() >= 1000){
-   if (scrollToTop.is(':hidden')){
-      scrollToTop.fadeIn(400);
-   }
- } else {
-   scrollToTop.fadeOut(400);
- }
-});
-
-$('.scroll-to-top').click(function(event){
-event.preventDefault();
-$('html, body').animate({
-  scrollTop: 0
-}, 1000)
-$(this)
-});
+  $(window).scroll(function (){
 
     var scrollToTop = $('.scroll-to-top');
- if ($(window).scrollTop() >= 1000){
-   if (scrollToTop.is(':hidden')){
-      scrollToTop.fadeIn(400);
-   }
- } else {
-   scrollToTop.fadeOut(400);
- }
-});
+     if ($(window).scrollTop() >= 1000){
+       if (scrollToTop.is(':hidden')){
+          scrollToTop.fadeIn(400);
+       }
+     } else {
+       scrollToTop.fadeOut(400);
+     }
+  });
+
+  $('.scroll-to-top').click(function(event){
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1000)
+    $(this)
+  });
 
 });
+
+
+// JS
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("column");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("actives");
+    current[0].className = current[0].className.replace(" actives", "");
+    this.className += " actives";
+  });
+}
